bdfbf01e4817ef760ec098accdb61073a7254df0
youssef elhoubi <elhoubiyoussef@gmail.com>
2025-02-19 11:20:13 +0100

    created a model for the post and a nother one for edited some component in blade

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,7 +2,7 @@
     <div class="container mx-auto px-4 py-2 flex items-center justify-between">
         <!-- Left -->
         <div class="flex items-center">
-            <h1 class="text-blue-600 text-4xl font-bold mr-4">facebook</h1>
+            <img src="https://res.cloudinary.com/dxbvwgxvf/image/upload/v1739959656/rtck5tmunfzfaemtcdut.png" alt="platform logo" class="w-28 object-fill">
             <div class="relative">
                 <i class="fas fa-search absolute left-3 top-3 text-gray-400"></i>
                 <input type="text" placeholder="Search Facebook" class="pl-10 pr-4 py-2 bg-gray-100 rounded-full w-64">
@@ -30,18 +30,8 @@
         
         <!-- Right -->
         <div class="flex items-center space-x-2">
-            <button class="p-2 hover:bg-gray-200 rounded-full">
-                <i class="fas fa-bars text-xl"></i>
-            </button>
-            <button class="p-2 hover:bg-gray-200 rounded-full">
-                <i class="fab fa-facebook-messenger text-xl"></i>
-            </button>
-            <button class="p-2 hover:bg-gray-200 rounded-full">
-                <i class="fas fa-bell text-xl"></i>
-            </button>
-            <button class="p-2 hover:bg-gray-200 rounded-full">
-                <img src="/api/placeholder/40/40" alt="Profile" class="w-10 h-10 rounded-full">
-            </button>
+            
+            
         </div>
     </div>
 </header>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\post;
 
 Route::get('/', function () {
     return view('welcome');
